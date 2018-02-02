8b6c169442cdff08e29b860d4ef6b6816afe7a9a
Jeff <pzping@gmail.com>
2018-02-02 12:25:58 +0800

    add app.py app.ini files

---
+++ app.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/env python2
+# -*- coding:utf-8 -*-
+
+import sys
+import httplib
+import urllib
+import socket
+import requests
+import time
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def application(environ, start_response):
+    status = "200 OK"
+    output = "hello zppan"
+
+    response_headers = [("Content-type", "text/plain"),
+            ("Content-Length", str(len(output)))]
+    start_response(status, respone_headers)
+
+    return [output]
